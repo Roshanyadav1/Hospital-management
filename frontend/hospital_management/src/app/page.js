e6da921f66d226849bbadad1f473be0f8a40d6c1
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-10-24 21:34:00 +0530

    add haris

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-    <h1>Hello Megha</h1>
+    <h1>Hello haris</h1>
           </main>
 
   )
