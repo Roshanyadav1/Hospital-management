aa520ad0e1baee152e6c5f5985ba27c3ccdf9538
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-10-25 16:06:55 +0530

    Doctor Api Commited

--- backend/doctor/models.py
+++ backend/doctor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid 
 
 # Create your models here.
+class Doctor(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) 
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    specialty = models.CharField(max_length=200)
+    contact_number = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    password2 = models.CharField(max_length=100)
--- backend/doctor/urls.py
+++ backend/doctor/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from doctor.views import *
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('profile/register', DoctorRegistrationView.as_view()),
+    path('profile/view', DoctorProfileView.as_view()),
+    path('profile/view/<uuid:pk>', DoctorProfileView.as_view()),
+    path('profile/update/<uuid:pk>', DoctorProfileUpdateView.as_view()),
+    path('profile/delete/<uuid:pk>', DoctorProfileDeleteView.as_view()),
+    path('profile/login', DoctorProfileLoginView.as_view()),
 ]
