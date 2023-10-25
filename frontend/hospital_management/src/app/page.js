1415adc4aba6e7f647576f6332f18312479ce62c
roshan <roshan.yadav@baelworks.com>
2023-10-25 11:18:55 +0530

    test

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,3 +1,4 @@
+import { TextField } from "@mui/material";
 import Switch from "@mui/material/Switch";
 
 export default function Home() {
@@ -5,6 +6,9 @@ export default function Home() {
     <main>
       <h1>Material ui and Redux !</h1>
       <Switch />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
     </main>
   )
 }
