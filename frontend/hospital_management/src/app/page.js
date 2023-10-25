9f5e29a087fb7c6a9c0b9f2dede63106b6cdf542
roshan <roshan.yadav@baelworks.com>
2023-10-25 11:59:49 +0530

    changes

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,7 +1,22 @@
-import { TextField } from "@mui/material";
+"use client";
+import { Button, TextField } from "@mui/material";
 import Switch from "@mui/material/Switch";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();//
+
+
+  // const sayHello = () => {
+  //   console.log("hello");
+  // }
+  
+
+  const navigateToDashboard = () => {
+    router.push("/dashboard");
+  }
+
   return (
     <main>
       <h1>Material ui and Redux !</h1>
@@ -9,6 +24,8 @@ export default function Home() {
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
+
+      <Button onClick={navigateToDashboard} variant="contained">Go to dashboard</Button>
     </main>
   )
 }
