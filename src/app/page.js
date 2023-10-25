18ab64d7b1ceda84d6c548cfb175ba281e993655
Prashant-Y <prashantydv2409@gmail.com>
2023-10-25 15:05:29 +0530

    I've complted my task (home page having input fields and the data will be shown on screen after submit)

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+import styles from './page.module.css'
+import Hello from './user/hello'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Hello/>
+          </main>
+  )
+}
