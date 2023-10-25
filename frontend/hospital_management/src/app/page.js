6297182979ce792260644b064611f8c6c9f09069
roshan <roshan.yadav@baelworks.com>
2023-10-25 11:06:49 +0530

    added material ui

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,7 +1,10 @@
+import Switch from "@mui/material/Switch";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World !</h1>
+      <h1>Material ui and Redux !</h1>
+      <Switch />
     </main>
   )
 }
