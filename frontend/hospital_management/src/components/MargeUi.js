260625bb3f1a2e6b8b829de21ec5cf5ea6de8e04
Megha <meghadhangar1301@gmail.com>
2023-10-31 04:27:43 +0530

    create navbar sidebar and footer

---
+++ frontend/hospital_management/src/components/MargeUi.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Footer from "@/components/Footer";
+import MiniDrawer from '@/components/Sidebar';
+import DrawerAppBar from '@/components/Navbar'; 
+
+function MargeUi() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MargeUi
