e2ada989cfc500454a8f1bd83d2a5a3f5c63ebcd
roshan <roshan.yadav@baelworks.com>
2023-10-30 10:32:01 +0530

    add package

--- frontend/hospital_management/src/app/login/page.js
+++ frontend/hospital_management/src/app/login/page.js
@@ -1,8 +1,23 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>this is login</h1>
-    </main>
-  )
-}
+"use client"
+import { Button, Container, Input } from '@mui/material';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
+export default function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
+  return (
+    // create a big container in mui 
+    <Container maxWidth="sm">
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input type="text" placeholder="userName" {...register("userName", { required: true })} />
+        <Input type="text" placeholder="password" {...register("password", { required: true })} />
+
+        <Button type="submit" variant="contained">Login</Button>
+      </form>
+    </Container>
+  );
+}
