b92da3c2513558294f8c28be47c28dc89208ffb5
Aditi-Agrawal-04 <148543765+Aditi-Agrawal-04@users.noreply.github.com>
2023-11-02 14:44:52 +0530

    Delete backend directory

--- backend/Hospital/models.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from django.db import models
-
-# Create your models here.
-class Hospital(models.Model):
-    uid =models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
--- backend/Hospital/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Hospital
-from rest_framework import serializers
-
-class HospitalSerializer(serializers.ModelSerializer): 
-    class Meta:
-        model = Hospital
-        fields = '__all__'
-        
