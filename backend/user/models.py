068bd9b5b0277650b206f7bd666fb29dff0c08da
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-11-03 16:22:05 +0530

    Email Field Added

--- backend/user/models.py
+++ backend/user/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     user_id  = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     member_id = models.UUIDField(default = uuid.uuid4, editable = False)
     username = models.CharField(max_length = 255)
+    user_email = models.EmailField(max_length = 255)
     password = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
