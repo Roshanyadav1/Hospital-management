c1b509cf7d8163db028451912bd357f7db56b8e7
Megha <meghadhangar1301@gmail.com>
2023-11-06 10:15:48 +0530

    footer

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import FetchData from '@/app/data/page'
 import Link from 'next/link'
-import Footer from '@/components/Footer'
 function page() {
   return (
     <div>
-
-     <Footer/>
-  
-    {/* //  <h1>Page component</h1>
-    //  <Link href='/data'>Go To Fetching data page</Link> */}
+     <h1> Welcome Our First page</h1>
+     <Link href='/data'>Go To Fetching data page</Link>
     </div>
   )
 }
--- frontend/hospital_management/src/components/auth.js
+++ frontend/hospital_management/src/components/auth.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Typography, Paper, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
