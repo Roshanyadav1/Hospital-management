f73221dba1e73c8b9df14587af60ad70dbfb9d7d
harshita-ydv <harshitay.bba2022@ssism.org>
2023-11-06 16:34:59 +0530

    Steper  task complete

--- frontend/hospital_management/src/components/Steper.js
+++ frontend/hospital_management/src/components/Steper.js
@@ -17,25 +17,19 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const images = [
   {
     imgPath:
-      //   'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-      "https://img.freepik.com/free-photo/doctor-with-stethoscope-hands-hospital-background_1423-1.jpg?w=1060&t=st=1699076591~exp=1699077191~hmac=d4cc8a1ca67c44d5a7bc7e64b9a10d77e081f8984863ad8d397c6086fe198b79",
-    //    './assest/logo_make_11_06_2023_324.jpg',
-    //
-  },
+       "https://img.freepik.com/free-photo/doctor-with-stethoscope-hands-hospital-background_1423-1.jpg?w=1060&t=st=1699076591~exp=1699077191~hmac=d4cc8a1ca67c44d5a7bc7e64b9a10d77e081f8984863ad8d397c6086fe198b79",
+     },
   {
     imgPath:
-      //   'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
       "https://img.freepik.com/premium-photo/doctor-hold-icon-health-electronic-medical-record-interface-digital-healthcare-network_34200-712.jpg?w=1380",
   },
   {
     imgPath:
-      //   'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-      "https://images.unsplash.com/photo-1624343285636-aba82fd5a124?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+       "https://images.unsplash.com/photo-1624343285636-aba82fd5a124?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     imgPath:
-      //   'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
       "https://img.freepik.com/premium-photo/modern-commercial-architecture-glass-steel-forum-skyscraper-reflecting-blue-sky_250132-16918.jpg?w=1060",
   },
 ];
@@ -99,32 +93,15 @@ function SwipeableTextMobileStepper() {
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
-        
-     steps={maxSteps}
+        steps={maxSteps}
         position="static"
         activeStep={activeStep}
         nextButton={
-          <Button
-            size="small"
-            // onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            {/* Next */}
-            {/* {theme.direction === 'rtl' ? ( */}
-            {/* //   <KeyboardArrowLeft /> */}
-            {/* // ) : ( */}
-            {/* //   <KeyboardArrowRight /> */}
-            {/* )} */}
-          </Button>
+          <Button size="small" disabled={activeStep === maxSteps - 1}></Button>
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {/* {theme.direction === 'rtl' ? ( */}
-            {/* <KeyboardArrowRight /> */}
-            {/* : ( */}
-            {/* <KeyboardArrowLeft /> */}
-            {/* ) */}
-            {/* Back */}
+            {" "}
           </Button>
         }
       />
--- frontend/hospital_management/src/components/container.js
+++ frontend/hospital_management/src/components/container.js
@@ -2,40 +2,178 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import { Grid } from "@mui/material";
-
+import { Grid, Typography, Button } from "@mui/material";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import container from '@/components/container'
 export default function FixedContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
 
       <Container
-        // fixed
         sx={{
-          width: "100wh",
-        //   margin: "0px",
+          width: "100%",
+          height:'20%',
+          marginTop:'2rem',
+         textAlign: "center",
         }}
       >
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
+          
         >
-            <Grid item xs={12}  md={3}>
-            <h1>xs=1</h1>
-             </Grid>
-          <Grid item xs={12}  md={3}>
-            <h1>xs=2</h1>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              padding: 2,
+              backgroundColor: "#13293d",
+              color: "white",
+            }}
+          >
+            <MedicalServicesIcon sx={{ fontSize: 48 }}/>
+            <Typography variant="h6" sx={{ padding: 3 }}>
+              Book an Appoinment{" "}
+            </Typography>
+            <Typography variant="body" sx={{ padding: 3}}>
+              +9876543467 <br></br><br></br>{" "}
+            </Typography>
+            <Typography variant="body" sx={{ padding: 1 }}>
+              OPD TIMING <br></br>{" "}
+            </Typography>
+            <Typography variant="body2" sx={{ padding: 1 }}>
+              10:00AM to 12:30PM
+            </Typography>
+            <Typography variant="body2">10:00AM to 12:30PM</Typography>
+
+            <Button
+              variant="contained"
+              sx={{
+                margin: 6,
+                backgroundColor: "white",
+                color: "#13293d",
+                padding:'10px',
+                "&:hover": {
+                  backgroundColor: "#A7AFB7",
+                  
+                },
+              }}
+            >
+              Reach Us
+            </Button>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              padding: 2,
+              backgroundColor: "#A7AFB7",
+              color: "white",
+            }}
+          >
+            <VolunteerActivismIcon sx={{ fontSize: 48 }} />
+            <Typography variant="h6" sx={{ padding: 2 }}>
+              Specialities & Treatments{" "}
+            </Typography>
+            <Typography variant="body2" sx={{ padding: 2 }}>
+              Experience the best-in-class medical treatments with over 30+
+              specialities.
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                margin: 7,
+                backgroundColor: "white",
+                color: "#13293d",
+                padding:'10px',
+                "&:hover": {
+                  backgroundColor: "#A7AFB7",
+                 
+                },
+              }}
+            >
+              know More
+            </Button>
           </Grid>
 
-          <Grid item xs={12}  md={3}>
-            <h1>xs=3</h1>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              padding: 2,
+              backgroundColor: "#13293d",
+              color: "white",
+              
+            }}
+          >
+            <PersonAddIcon sx={{ fontSize: 48 }} />
+            <Typography variant="h6" sx={{ padding: 2 }}>
+              Find a Doctor{" "}
+            </Typography>
+            <Typography variant="body2" sx={{ padding: 4 }}>
+              Trust the expert care of our doctors for your health and
+              well-being.
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                margin: 7,
+                backgroundColor: "white",
+                color: "#13293d",
+                padding:'10px',
+                "&:hover": {
+                  backgroundColor: "#A7AFB7",
+                 
+                },
+              }}
+            >
+              know More
+            </Button>
           </Grid>
 
-          <Grid item xs={12}  md={3}>
-            <h1>xs=4</h1>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{
+              padding: 2,
+              backgroundColor: "#A7AFB7",
+              color: "white",
+             
+            }}
+          >
+            <HealthAndSafetyIcon sx={{ fontSize: 48 }} />
+            <Typography variant="h6" sx={{ padding: 2 }}>
+              Health Checkup Packages{" "}
+            </Typography>
+            <Typography variant="body2" sx={{ padding: 2 }}>
+              "An executive checkup a year, keeps your future hospital bill in
+              control"
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                margin: 7,
+                backgroundColor: "white",
+                color: "#13293d",
+                padding:'10px',
+                "&:hover": {
+                  backgroundColor: "#A7AFB7",
+                  
+                }
+              }}
+            >
+              Visit know
+            </Button>
           </Grid>
-
         </Grid>
       </Container>
     </React.Fragment>
