3158a2e4b7f2863536a297ef05804c9a0e079ac7
Megha <meghadhangar1301@gmail.com>
2023-11-08 13:29:27 +0530

    add SGA logo in footer

--- frontend/hospital_management/src/components/Footer.js
+++ frontend/hospital_management/src/components/Footer.js
@@ -4,42 +4,45 @@ import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import './footer.css';
-// import Image from "next/image";
+import Image from "next/image";
 // import instaa from '../assest/instaa.png';
 // import FB from '../assest/fb.png';
 // import Twiter from '../assest/twiter.png';
+import Logo from '../assest/whiteSga.png';
 const Footer = () => {
   return (
-    <Box className="main-body"
-      component="footer"
+    <Grid className="main-body"
+      // component="footer"
       sx={{
         backgroundColor: "#13293D",
         color: "#fff",
-        padding: 2,
+        paddingTop: 2,
         textAlign: "center",
-        position: "absolute",
-        width: "100vw",
-        bottom:0,
-        left:0,
+        // position: "absolute",
+        // width: "100vw",
+        // bottom:0,
+        // left:0,
       }}
     >
-      <Container maxWidth="lg">
+      <Container sx={{margin:'0px'}} maxWidth="lg">
         <Grid container spacing={2}>
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" sx={{ padding: 2 }}>
-              Hospital Application
-            </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 }}>
+            {/* <Typography variant="h6" sx={{border:"1px solid white"}} > */}
+
+              <Image  width={160} height={50}  src={Logo}/>
+            {/* </Typography> */}
+             
+            {/* <Typography variant="body2" color="white" sx={{ padding: 1 }}>
               We are XYZ company, dedicated to providing the best service to our
               customers.
-            </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1}}>
+            </Typography> */}
+            {/* <Typography variant="body2" color="white" sx={{ padding: 1}}>
              Head- Nilesh Giri, Contact Number,+919691070767 | Roshan Yadav ,Keshav Vishvakarma
-            </Typography>
+            </Typography> */}
           </Grid>
 
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" sx={{ padding: 1 }}>
+            <Typography variant="body1" sx={{ padding: 1 }}>
               Social Media
             </Typography>
             <Link href="https://www.facebook.com" color="inherit">
@@ -58,80 +61,59 @@ const Footer = () => {
           </Grid>
 
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" color="white" sx={{ padding: 1 }}>
+            <Typography variant="body1" color="white" sx={{ padding: 1 }}>
               Centres of Excellence
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                  "&:hover": {
-                  borderBottom: "1px solid white",
+            <Typography variant="body2" color="white" sx={{ padding: 1 
                  
-                  }
             
             }}>
               Centre for Neurosciences
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                  "&:hover": {
-                    borderBottom: "1px solid white",
-                    }
+            <Typography variant="body2" color="white" sx={{ padding: 1 
+                  
             }}>
               Centre for Transplant
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                "&:hover": {
-                  borderBottom: "1px solid white",
-                  }
+            <Typography variant="body2" color="white" sx={{ padding: 1 
+                
             }}>
               Critical Care Unit
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1  ,
-                    "&:hover": {
-                  borderBottom: "1px solid white",
-              
-                  }
+            <Typography variant="body2" color="white" sx={{ padding: 1  
+                   
             }}>
               Department of Cardiology
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                    "&:hover": {
-                  borderBottom: "1px solid white",
-                  
-                  }
+            <Typography variant="body2" color="white" sx={{ padding: 1 
+                   
             }}>
               Department of Orthopedics
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                    "&:hover": {
-                  borderBottom: "1px solid white",
-                
-                  }
+            <Typography variant="body2" color="white" sx={{ padding: 1 
+                  
             }}>
               Gynaecology and Obstetrics
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                    "&:hover": {
-                  borderBottom: "1px solid white",
-                 
-                  }
+            <Typography variant="body2" color="white" sx={{ padding: 1
+                  
             }}>
               Pediatrics
             </Typography>
           </Grid>
 
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" color="white" sx={{ padding: 1 }}>
+            <Typography variant="body1" color="white" sx={{ padding: 1 }}>
               Hospital Details
             </Typography>
-            <Typography variant="body2" color="white" sx={{ padding: 1 ,
-                     "&:hover": {
-                      borderBottom: "1px solid white",
-                      
-                      }
+            <Typography variant="body2" color="white" sx={{ padding: 1 
+                    
             }}>
               Contact Us
             </Typography>
             <Typography variant="body2" color="white" sx={{ padding: 1 }}>
-              <Input
+              <Input 
+               varient="outline"
                 type="text"
                 placeholder="Enter your email"
                 sx={{
@@ -160,11 +142,11 @@ const Footer = () => {
       </Grid>
 
       <hr />
-      <Typography variant="body2" sx={{ padding: 2 }}>
+      <Typography variant="body2" sx={{ padding: 2, display: "flex", justifyContent: "center" ,marginLeft:5 }}>
         &copy; {new Date().getFullYear()} Your Website Name
       </Typography>
     </Container>
-    </Box >
+    </Grid >
   );
 };
 
