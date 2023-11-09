f5088f67cce9e3ca29d203cee107eae928992150
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-11-09 13:59:44 +0530

    changes

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,7 +1,15 @@
+"use client"
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function Home() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <main>
       <h1>this is home</h1>
+      <header className="App-header">
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      </header>
     </main>
   )
 }
