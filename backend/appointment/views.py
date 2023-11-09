7f4236fd9b1411a6904f2ef96c7d3fa608ad6462
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-11-09 13:41:52 +0530

    Appointment filter and pagination added

--- backend/appointment/views.py
+++ backend/appointment/views.py
@@ -1,16 +1,19 @@
-from appointment.models import Appointment
+from rest_framework.pagination import PageNumberPagination
+from appointment.serializers import *
 from rest_framework.generics import GenericAPIView
-from appointment.serializers import AppointmentSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
+from appointment.models import Appointment
 from rest_framework.views import APIView
 from rest_framework import status
 
 
 class AppointmentAdd(GenericAPIView):
-    serializer_class = AppointmentSerializer
+    serializer_class = AppointmentAddSerializer
 
     def post(self, request, format = None):
-        serializer = AppointmentSerializer(data = request.data)
+        serializer = AppointmentAddSerializer(data = request.data)
         serializer.is_valid(raise_exception = True)
         serializer.save()
         return Response(
@@ -20,7 +23,15 @@ class AppointmentAdd(GenericAPIView):
             },
         )
 
-class AppointmentView(APIView):
+class AppointmentView(ListAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    filterset_fields = ['appointment_date', 'appointment_time', 'patient_id', 'doctor_id',]
+    pagination_class  = PageNumberPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['appointment_date', 'appointment_time', 'patient_id', 'doctor_id',]
+
+class AppointmentViewById(APIView):
     def get(self, request, input = None, format = None):
         id = input
         if id is not None:
@@ -41,16 +52,6 @@ class AppointmentView(APIView):
                         'message': "Appointment Is Not Booked",
                     },
                 ) 
-        else:
-            appointment = Appointment.objects.all()
-            serializer = AppointmentSerializer(appointment, many = True)
-            return Response(
-                {
-                    'status': status.HTTP_200_OK,
-                    'message': "Appointments Data Retrieved Successfully",
-                    'data': serializer.data
-                },
-            )
 
 class AppointmentUpdate(APIView):
     def put(self, request, input, format = None):
