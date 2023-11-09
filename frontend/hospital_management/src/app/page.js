b3bb226a1881e1b549177877b6fbe00f81cfb3d5
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-11-09 14:18:30 +0530

    changes.2

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,18 +1,24 @@
 "use client"
 import { useAuth0 } from "@auth0/auth0-react";
-// import SteperNav from "@/components/SteperNav";
-// import FixedContainer from "@/components/container";
+import SteperNav from "@/components/SteperNav";
+import FixedContainer from "@/components/container";
 
 export default function Home() {
   const { loginWithRedirect } = useAuth0();
 
   return (
+    <>
+    <div>
+           <SteperNav />
+           <FixedContainer />
+        </div>
     <main>
       <h1>this is home</h1>
       <header className="App-header">
         <button onClick={() => loginWithRedirect()}>Log In</button>
       </header>
     </main>
+    </>
   )
 }
 
