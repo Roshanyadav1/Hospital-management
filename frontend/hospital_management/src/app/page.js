31b97f7e44dddc68baf52a8ada20603c1b09c1eb
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-11-09 15:00:50 +0530

    deletion of swipeable text in harshta_dev

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import FetchData from '@/app/data/page'
 import Link from 'next/link'
-import SwipeableTextMobileStepper from '@/components/Steper'
+// import SwipeableTextMobileStepper from '@/components/Steper'
 import { NavigateBeforeRounded } from '@mui/icons-material'
 //  import SteperNav from '@/components/SteperNav'
 //  import FixedContainer from '@/components/container'
