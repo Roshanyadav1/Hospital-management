ddf6cb7ce1abc8bab7797ae6083234d4a5994ca5
roshan <roshan.yadav@baelworks.com>
2023-11-17 19:02:25 +0530

    change in design

--- frontend/hospital_management/src/app/dashboard/layout.js
+++ frontend/hospital_management/src/app/dashboard/layout.js
@@ -1,9 +1,6 @@
 "use client"
 
-import { Inter } from 'next/font/google'
 import MiniDrawer from '@/components/sidebar'
-import Navbar from "@/components/Navbar"; // Import your navbar component
-import Footer from "@/components/Footer"
 import { Box } from "@mui/material";
 import React from 'react'
 function layout({children}) {
@@ -16,4 +13,6 @@ function layout({children}) {
     </>
   )
 }
+
+
 export default layout
