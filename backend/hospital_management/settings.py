f7e29b2835d0f97555e44e56528003df531f2f5f
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-11-20 14:58:18 +0530

    API - logger removed due to vercel errors

--- backend/hospital_management/settings.py
+++ backend/hospital_management/settings.py
@@ -188,37 +188,37 @@ SIMPLE_JWT = {
     "JTI_CLAIM": "jti",
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
-            'format': '%(service)s %(ipAddress)s %(correlationId)s %(user)s %(url)s %(requestType)s %(traceparent)s %(asctime)s %(levelname)s %(name)s %(message)s %(method)s %(entryTypeEnum)s',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': 'django.log',
-            'formatter': 'verbose',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
+#             'format': '%(service)s %(ipAddress)s %(correlationId)s %(user)s %(url)s %(requestType)s %(traceparent)s %(asctime)s %(levelname)s %(name)s %(message)s %(method)s %(entryTypeEnum)s',
+#         },
+#         'simple': {
+#             'format': '{levelname} {message}',
+#             'style': '{',
+#         },
+#     },
+#     'handlers': {
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': 'django.log',
+#             'formatter': 'verbose',
+#         },
+#         'console': {
+#             'level': 'INFO',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file', 'console'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         },
+#     },
+# }
