0d3c6c259c02992968749a9688420100a5b85f44
Megha <meghadhangar1301@gmail.com>
2023-11-21 21:31:15 +0530

    update code

---
+++ frontend/hospital_management/src/app/Doctor/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Doctor from '@/components/Doctor'
+function DoctorPage() {
+  return (
+    <div>
+      <Doctor/>
+    </div>
+  )
+}
+
+export default DoctorPage
