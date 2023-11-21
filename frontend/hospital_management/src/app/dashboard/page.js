06580262a6a69eca3f7aa2d131e91dae4cce718e
roshan <roshan.yadav@baelworks.com>
2023-11-21 12:18:01 +0530

    changes in layout

--- frontend/hospital_management/src/app/dashboard/page.js
+++ frontend/hospital_management/src/app/dashboard/page.js
@@ -1,13 +1,8 @@
 import DataTable from "@/components/DataTable";
-import MiniDrawer from '@/components/sidebar'
 // when admin login then the dashboard will be shown
 function FetchData() {
   return (
-    <div style={{
-      margin: '5rem',
-    }}>
-      
-      <h1>Employee Table</h1>
+    <div>
       <DataTable />
     </div>
   )
