7b1b5811f41d4285bef624f8113f429878c49b56
Aman Yadav <virajgurjar789@gmail.com>
2023-11-22 22:47:14 +0530

    aws s3 photo uploading fixed

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import S3UploadForm from '@/app/components/S3UploadForm'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+       <S3UploadForm/>
+    </main>
+  )
+}
+  
