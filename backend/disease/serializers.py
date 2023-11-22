ed3a162d526e244d34befced07846e6c3da2934e
Aman Yadav <virajgurjar789@gmail.com>
2023-11-22 22:39:53 +0530

    aws s3 photo upload

--- backend/disease/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from disease.models import Disease
-
-class DiseaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Disease
-        exclude = ('created_by', 'updated_by', )
-        
