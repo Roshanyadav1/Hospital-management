831437cba5bc262cb9d506fb635fc0afb95dc4b8
Aman Yadav <virajgurjar789@gmail.com>
2023-11-22 17:14:59 +0530

    aws photo upload demo

--- frontend/hospital_management/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-    },
-}
-
-module.exports = nextConfig
