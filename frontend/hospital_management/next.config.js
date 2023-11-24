6c45cf7daada9861f54f132e187f7d92c190f4e1
harshita-ydv <harshitay.bba2022@ssism.org>
2023-11-24 14:19:24 +0530

    remove domain

--- frontend/hospital_management/next.config.js
+++ frontend/hospital_management/next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
         dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     },
     images: {
-<<<<<<< HEAD
         domains: ['fastly.picsum.photos' , 'www.carehospitals.com', 't4.ftcdn.net','media.istockphoto.com', 'medlinkstaffing.com', 'getwallpapers.com', 'www.stockvault.net','www.pngitem.com','thumbs.dreamstime.com' ,'img.freepik.com','_helpers_welcome__WEBPACK_IMPORTED_MODULE_3___default']
-=======
-        domains: ['fastly.picsum.photos' , 'img.freepik.com' , 'thumbs.dreamstime.com'],
->>>>>>> 1e0500cb69beacdbbfb9f63444c2076348e3e937
     },
 }
 
