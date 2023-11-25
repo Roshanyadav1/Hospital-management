82dba05999b30069eafa3a57e13e307c627e3592
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-11-25 11:48:56 +0530

    cors origin changes

--- backend/hospital_management/settings.py
+++ backend/hospital_management/settings.py
@@ -76,10 +76,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://192.168.0.7:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://192.168.0.7:8000",
+# ]
 
 ROOT_URLCONF = 'hospital_management.urls'
 MEDIA_ROOT = BASE_DIR/'media/'
