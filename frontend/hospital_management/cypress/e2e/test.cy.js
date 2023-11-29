f0791ac5c7b1507cdae80a571e7cca3dba6f2556
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-11-29 14:07:51 +0530

    cypress employee form testing

--- frontend/hospital_management/cypress/e2e/test.cy.js
+++ frontend/hospital_management/cypress/e2e/test.cy.js
@@ -1,9 +1,36 @@
 /// <reference types="cypress"/>
 
-it("google search" ,()=>{
 
-    cy.visit("https://google.com")
-    cy.get('#APjFqb').type("haris in urdu walpaper")
-    cy.get('.FPdoLc > center > .gNO89b').click()
-    cy.get('.MUFPAc > :nth-child(2) > a').click()
+
+//the code below is to ignore unhandeled hydration errors
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+
+  //Code to run tests
+  it("empsearch",()=>{
+    cy.visit("http://localhost:3000/dashboard/addEmployee")
+    cy.wait(2000)
+    //id me jhn jhn single slash h whn double slash legenge { eg = '#\:r1\:'   == '#\\:r1\\:' }
+    cy.get('#\\:r1\\:').type("haris")
+     cy.wait(2000)
+    cy.get('#\\:r2\\:').type("haris@gmail.com")
+     cy.wait(2000)
+    cy.get('#\\:r3\\:').type("9770099834")
+     cy.wait(2000)
+    cy.get('#\\:r4\\:').type("Haris@123")
+     cy.wait(2000)
+    cy.get(':nth-child(5) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiAutocomplete-popupIndicator > [data-testid="ArrowDropDownIcon"]').click()
+    cy.get('#\\:r5\\:-option-0').click()
+     cy.wait(2000)
+
+    cy.get(':nth-child(6) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiAutocomplete-popupIndicator > [data-testid="ArrowDropDownIcon"]').click()
+    cy.get('#\\:r7\\:-option-0').click()
+     cy.wait(2000)
+    cy.contains('Active').click()
+     cy.wait(2000)
+     cy.get(':nth-child(10) > .MuiButtonBase-root').click()
 })
+    
