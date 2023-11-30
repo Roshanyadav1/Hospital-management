acb6dc3862f7a131b2dc9964a255667aa95834c1
Megha <meghadhangar1301@gmail.com>
2023-11-30 10:43:09 +0530

    changes

--- frontend/hospital_management/src/app/docter/doctor3/page.js
+++ frontend/hospital_management/src/app/docter/doctor3/page.js
@@ -1,46 +1,96 @@
-import React from 'react'
-import { Grid, Card, CardContent, Typography, Button } from "@mui/material";
-import CardActions from "@mui/material/CardActions";
-import Container from "@mui/material/Container";
-import { CardActionArea, CardMedia } from "@mui/material";
-import Home from "@/app/dashboard/page";
-import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useSpring, animated } from '@react-spring/web';
 
-function page() {
-  return (
-    <div>
-        <Grid container spacing={5} style={{ marginTop: "20px" }}>
-           <Grid item xs={12} md={4} sm={3}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  sx={{ height: 140, width:400, height:400 }}
-                 
-                    image="https://thumbs.dreamstime.com/b/doctor-portrait-21332357.jpg"
-                  //   title="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Dr. Sumit Jain
-                    <Typography variant="body2" color="text.secondary">
-                      
-                      DrNB (Padiatric Cardiology)
-                      <Typography variant="body3" color="text.secondary">
-                       paediatric Cardiology
-                       
-                      </Typography>
-                    </Typography>
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="large"> Learn More</Button>  
-                </CardActions>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          </Grid>
-    </div>
-  )
+interface FadeProps {
+  children: React.ReactElement;
+  in?: boolean;
+  onClick?: any;
+  onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
+  onExited?: (node: HTMLElement, isAppearing: boolean) => void;
+  ownerState?: any;
 }
 
-export default page
+const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(props, ref) {
+  const {
+    children,
+    in: open,
+    onClick,
+    onEnter,
+    onExited,
+    ownerState,
+    ...other
+  } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter(null as any, true);
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited(null as any, true);
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {React.cloneElement(children, { onClick })}
+    </animated.div>
+  );
+});
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function SpringModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            TransitionComponent: Fade,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="spring-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="spring-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
