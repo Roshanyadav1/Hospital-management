936e4faf6c26ae64ea300cd333b8f386a49439eb
Mohammad-Haris24 <mo.haris7999@gmail.com>
2023-11-30 16:19:14 +0530

    first commit

--- frontend/hospital_management/src/components/DoctorPage.js
+++ frontend/hospital_management/src/components/DoctorPage.js
@@ -1,166 +1,187 @@
-"use client"
-import * as React from 'react';
-import { useState } from 'react';
-import dayjs from 'dayjs';
-import { DemoItem } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import Grid from '@mui/system/Unstable_Grid/Grid';
-import Container from '@mui/material/Container';
-import { Typography, Button, TextField } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
-import { useGetSpecialistDoctorQuery } from '@/services/Query';
-
-
-
-
-
+'use client'
+import * as React from 'react'
+import { useState } from 'react'
+import dayjs from 'dayjs'
+import { DemoItem } from '@mui/x-date-pickers/internals/demo'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
+import Grid from '@mui/system/Unstable_Grid/Grid'
+import Container from '@mui/material/Container'
+import { Typography, Button, TextField } from '@mui/material'
+import Autocomplete from '@mui/material/Autocomplete'
+import { useGetSpecialistDoctorQuery } from '@/services/Query'
+import Doctor from './Doctor'
 
 function DoctorPage() {
-  
+   const styles = {
+      container: {
+         backgroundImage: `url(${'https://e0.pxfuel.com/wallpapers/597/471/desktop-wallpaper-hospital-medical-care.jpg'})`,
+         backgroundSize: 'cover',
+         backgroundRepeat: 'no-repeat',
+         backgroundPosition: 'center',
+         color: 'white', // Adjust text color based on your background
+         padding : '2rem' ,
+         display: 'flex',
+         alignItems: 'center',
+         justifyContent: 'center',
+      },
+   }
 
+   const diseases = ['diabetes', 'thyroid', 'dengue', 'malaria']
+   const doctor = ['haris', 'tuba', 'shahbaaz', 'arshi']
 
-  const styles = {
-    container: {
-      backgroundImage: `url(${"https://t4.ftcdn.net/jpg/06/23/97/77/240_F_623977732_Qr9vyosS3vSzQoIWAzwfYQYrV8LpPHPM.jpg"})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      color: 'white', // Adjust text color based on your background
-      height: '60vh', // Adjust the height as needed
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  };
+   //const [stringArray, setStringArray] = useState(['diabetes', 'thyroid', 'malaria']);
 
-  const diseases = ['diabetes', 'thyroid', 'dengue', 'malaria',]
+   const [selectedDate, setSelectedDate] = useState(dayjs('2023-11-17')) // Initial date value
+   const [selectedDiseases, setSelectedDiseases] = useState([]) // Initial diseases value
+   const [selectedDoctor, setSelectedDoctor] = useState([]) // Initial diseases value
 
-  //const [stringArray, setStringArray] = useState(['diabetes', 'thyroid', 'malaria']);
+   const handleDateChange = date => {
+      setSelectedDate(date)
+   }
 
-  const [selectedDate, setSelectedDate] = useState(dayjs('2023-11-17')); // Initial date value
-  const [selectedDiseases, setSelectedDiseases] = useState([]); // Initial diseases value
+   const handleDiseasesChange = (event, values) => {
+      setSelectedDiseases(values)
+   }
 
+   const handleDoctorChange = (event, values) => {
+      setSelectedDoctor(values)
+   }
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+   const handleSubmit = event => {
+      event.preventDefault()
 
-  const handleDiseasesChange = (event, values) => {
-    setSelectedDiseases(values);
-    
-  };
+      // Now you can use selectedDate and selectedDiseases in your fetch or any other logic
+      console.log('Selected Date:', selectedDate)
+      console.log('Selected Diseases:', selectedDiseases)
+      console.log('Selected Doctor:', selectedDoctor)
+      // Add your fetch or other logic here
+   }
 
+   let fill = {
+      disease: selectedDate,
+      day: selectedDiseases,
+   }
+   const { data: docData } = useGetSpecialistDoctorQuery(fill)
 
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
+   const Typo = {
+      fontWeight: 800,
+      fontSize: '2.5rem',
+   }
 
-    // Now you can use selectedDate and selectedDiseases in your fetch or any other logic
-    console.log('Selected Date:', selectedDate);
-    console.log('Selected Diseases:', selectedDiseases);
+   console.log(docData, 'asd')
+   return (
+      <div>
+         <div style={styles.container}>
+            <Container maxWidth='lg'>
+               <Typography variant='h4' align='center' style={Typo}>
+                  Book Your Appointment
+               </Typography>
+               <form onSubmit={handleSubmit}>
+                  <Grid container spacing={5} style={{ marginTop: '1rem' }}>
+                     <Grid item xs={12} sm={3} md={3.5}>
+                        <Typography variant='body2' sx={{ marginBottom: '6px' }}>
+                           Select Disease
+                        </Typography>
 
-    // Add your fetch or other logic here
-  };
-  
-let fill = {
-    disease : selectedDate,
-    day:selectedDiseases,
-  }
-const {data :  docData} =useGetSpecialistDoctorQuery(fill)
+                        <Autocomplete
+                           freeSolo
+                           id='tags-outlined'
+                           options={diseases}
+                           value={selectedDiseases}
+                           onChange={handleDiseasesChange}
+                           sx={{
+                              background: 'white',
+                              outline: 'none',
+                              borderRadius: '5px',
+                           }}
+                           placeholder='diseases'
+                           disableClearable
+                           renderInput={params => (
+                              <TextField
+                                 {...params}
+                                 //  label="Search input"
+                                 InputProps={{
+                                    ...params.InputProps,
+                                    placeholder: 'diseases',
+                                    type: 'search',
+                                 }}
+                              />
+                           )}
+                        />
+                     </Grid>
 
-  const Typo = {
-    fontWeight: 800,
-    fontSize:"2.5rem",
-  }
-
-
-console.log(docData , "asd")
-  return (
-    <div>
-    <div style={styles.container} >
-      <Container maxWidth="lg">
-        <Typography variant='h4' align='center' style={Typo}>
-          Book Your Appointment
-        </Typography>
-        <form   onSubmit={handleSubmit}>
-          <Grid container spacing={5} style={{ marginTop: "1rem" }}>
-            <Grid item xs={12} sm={3} md={5} >
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoItem label="Select Date">
-                  <MobileDatePicker 
-                  defaultValue={dayjs('2023-11-17')}
-                  value={selectedDate}
-                  onChange={handleDateChange}
-                    sx={{ background: 'white',  borderRadius: '5px', }}
-                  />
-                </DemoItem>
-              </LocalizationProvider>
-            </ Grid>
-            <Grid item xs={12} sm={3} md={5} >
-              <Typography variant='body2' sx={{ marginBottom: "6px" }}>Select Disease</Typography>
-              <Autocomplete
-                multiple
-                id="tags-outlined"
-                options={diseases}
-                value={selectedDiseases}
-                onChange={handleDiseasesChange}
-
-                sx={{ background: 'white', outline: 'none', borderRadius: '5px', }}
-                placeholder='diseases'
-                getOptionLabel={(option) => option}
-                // defaultValue={[stringArray[13]]}
-                // filterSelectedOptions
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    placeholder="diseases"
-                  />
-                )}
-              />
-            </ Grid>
-            
-            <Grid item xs={12} sm={3} md={2} >
-              <Button variant="contained" size="large" type="submit"  sx={{marginTop:"25px" , height:"50px"}}>
-                submit
-              </Button>
-            </ Grid>
-          </ Grid>
-        </form>
-      </Container>
+                     <Grid item xs={12} sm={3} md={3.5}>
+                        <Typography variant='body2' sx={{ marginBottom: '6px' }}>
+                           Select Doctor
+                        </Typography>
+                        <Autocomplete
+                           freeSolo
+                           id='tags-outlined'
+                           options={doctor}
+                           value={selectedDoctor}
+                           onChange={handleDoctorChange}
+                           sx={{
+                              background: 'white',
+                              outline: 'none',
+                              borderRadius: '5px',
+                           }}
+                          
+                           disableClearable
+                           renderInput={params => (
+                              <TextField
+                                 {...params}
+                                 //  label="Search input"
+                                 InputProps={{
+                                    ...params.InputProps,
+                                    placeholder: 'doctor',
+                                    type: 'search',
+                                 }}
+                              />
+                           )}
+                        />
+                        
+                     </Grid>
+                     <Grid item xs={12} sm={3} md={3.5}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                           <DemoItem label='Select Date'>
+                              <MobileDatePicker
+                                 defaultValue={dayjs('2023-11-17')}
+                                 value={selectedDate}
+                                 onChange={handleDateChange}
+                                 sx={{ background: 'white', borderRadius: '5px' }}
+                              />
+                           </DemoItem>
+                        </LocalizationProvider>
+                     </Grid>
+                     <Grid item xs={12} sm={3} md={1.5}>
+                        <Button
+                           variant='contained'
+                           size='large'
+                           type='submit'
+                           sx={{ marginTop: '25px', height: '50px' }}
+                        >
+                           submit
+                        </Button>
+                     </Grid>
+                  </Grid>
+               </form>
+            </Container>
+         </div>
+         <div>
+            {docData?.data?.map((info, i) => (
+               <div key={i}>
+                  <h2>
+                     {info.doctor_id} {info.disease_specialist}
+                  </h2>
+                  <p>{info.day}</p>
+                  <p>{info.disease_specialist}</p>
+                  <hr></hr>
+               </div>
+            ))}
+         </div>
       </div>
-       <div>
-       {
-      docData?.data?.map((info,i)=>
-      <div key={i}>
-          <h2>{info.doctor_id} {info.disease_specialist}</h2>
-          <p>{info.day}</p>
-          <p>{info.disease_specialist}</p>
-          <hr></hr>
-        </div>
-      )
-    }
-     
-    </div>
-    </div>
-  )
+   )
 }
 export default DoctorPage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
