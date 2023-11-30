01d1599909be4230c47d9e0797b179609a5efa19
Aditi-Agrawal-04 <aditia.bca2022@ssism.org>
2023-11-30 12:50:51 +0530

    remove login view

--- backend/user/views.py
+++ backend/user/views.py
@@ -9,12 +9,6 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 
 
-def get_tokens_for_user(user):
-    refresh = RefreshToken.for_user(user)
-
-    return {
-        'access': str(refresh.access_token),
-    }
 
 
 class UserRegister(GenericAPIView):
@@ -52,58 +46,7 @@ class UserDelete(APIView):
         )
 
 
-class UserLoginView(GenericAPIView):
-    serializer_class = UserLoginSerializer
 
-    def post(self, request, format=None):
-        serializer = UserLoginSerializer(data=request.data)
-        email = request.data.get('user_email')
-        password = request.data.get('user_password')
-
-        if User.objects.filter(user_email=email).count() >= 1:
-            user = User.objects.get(user_email=email)
-            is_verify = request.data.get('is_verify')
-
-            if is_verify == 'true':
-                token = get_tokens_for_user(user)
-                return Response(
-                    {
-                        'status': status.HTTP_200_OK,
-                        'message': "Logged In As " + user.user_role,
-                        'data': {
-                            'user_role': user.user_role,
-                            'token': token,
-                        }
-                    },
-                )
-            else:
-                user = authenticate(user_email=email, password=password)
-                if user is not None:
-                    token = get_tokens_for_user(user)
-                    return Response(
-                        {
-                            'status': status.HTTP_200_OK,
-                            'message': "Logged In As " + user.user_role,
-                            'data': {
-                                'user_role': user.user_role,
-                                'token': token,
-                            }
-                        },
-                    )
-                else:
-                    return Response(
-                        {
-                            'status': status.HTTP_400_BAD_REQUEST,
-                            'message': "Password Mismatch",
-                        },
-                    )
-        else:
-            return Response(
-                {
-                    'status': status.HTTP_404_NOT_FOUND,
-                    'message': "Invalid User Id",
-                },
-            )
 
 # User Profile View
 
