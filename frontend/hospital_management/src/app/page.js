9b39448581626f1d51642eb54073dd41ef09f10b
Megha <meghadhangar1301@gmail.com>
2023-12-01 12:18:54 +0530

    changes

--- frontend/hospital_management/src/app/page.js
+++ frontend/hospital_management/src/app/page.js
@@ -4,20 +4,16 @@ import Footer from '@/components/Footer'
 import SteperNav from '@/components/SteperNav'
 import FixedContainer from '@/components/container'
 import { Grid } from '@mui/material' 
-import Doctor from '@/components/Doctor'
-import Cards from '@/components/Card'
+import Doctor from '@/components/Doctor';
 import SwipeableTextMobileStepper from '@/components/Steper'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // for the patient page , the patient page will be the main page for the patient
 
 function page() {
-const one={
-  backgroundColor:"#2CD9C51A"
-}
   return (
     
-    <div style={one}>
+    <div >
       <SteperNav />
       <SwipeableTextMobileStepper/>
       <Grid container item padding={3} >
