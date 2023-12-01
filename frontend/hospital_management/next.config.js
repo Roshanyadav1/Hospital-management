fddba27aeea926cc62815663f202f407566f172a
harshita-ydv <harshitay.bba2022@ssism.org>
2023-12-01 15:09:53 +0530

    changes

--- frontend/hospital_management/next.config.js
+++ frontend/hospital_management/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     images: {
         domains: ['fastly.picsum.photos' , 'img.freepik.com' , 'thumbs.dreamstime.com','next.config.js','img.freepik.com'],
         domains:['0 , _services_Query__WEBPACK_IMPORTED_MODULE_3__.useGetAppointmentQuery'],
-        domains:['img.freepik.com','t4.ftcdn.net','i.pinimg.com','media.istockphoto.com','cdn.create.vista.com','www.carehospitals.com']
+        domains:['img.freepik.com','t4.ftcdn.net','i.pinimg.com','media.istockphoto.com','cdn.create.vista.com','www.carehospitals.com', 'thumbs.dreamstime.com']
     },
 }
 
