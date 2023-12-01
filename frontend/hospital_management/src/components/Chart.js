d2d5227588d82599248b582f367408a4268fd3ab
Aditi-Agrawal-04 <aditia.bca2022@ssism.org>
2023-12-01 13:54:08 +0530

    merge chart changes in dashboard

--- frontend/hospital_management/src/components/Chart.js
+++ frontend/hospital_management/src/components/Chart.js
@@ -1,7 +1,15 @@
 // import React from 'react'
 'use client'
+// import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Grid } from "@mui/material"
+import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import Divider from '@mui/material/Divider';
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import Avatar from '@mui/material/Avatar';
+// import Typography from '@mui/material/Typography';
 import {
     ComposedChart,
     Line,
@@ -13,53 +21,56 @@ import {
     Legend,
     Area
 } from "recharts";
-import '@/components/container.css'
+import '@/components/App.css'
+import CommonListItem from './CommonListItem';
+import Image from 'next/image'
+import Doc from './Doc.png'
 
 const data = [
     {
         name: "Sun",
-        Pat: 29,
-        App: 15,
-        Doc: 4,
+        Patients: 29,
+        Appoints: 15,
+        Doctors: 4,
     },
     {
         name: "Mon",
-        Pat: 32,
-        App: 32,
-        Doc: 5,
+        Patients: 32,
+        Appoints: 32,
+        Doctors: 5,
     },
     {
         name: "Tues",
-        Pat: 38,
-        App: 33,
-        Doc: 4,
+        Patients: 38,
+        Appoints: 33,
+        Doctors: 4,
     },
     {
         name: "Wed",
-        Pat: 32,
-        App: 15,
-        Doc: 7,
+        Patients: 32,
+        Appoints: 15,
+        Doctors: 7,
     },
-    
+
     {
         name: "Thurs",
-        Pat: 22,
-        App: 19,
-        Doc: 3,
+        Patients: 22,
+        Appoints: 19,
+        Doctors: 3,
     },
     {
         name: "Fri",
-        Pat: 26,
-        App: 9,
-        Doc: 5,
+        Patients: 26,
+        Appoints: 9,
+        Doctors: 5,
     },
     {
         name: "Sat",
-        Pat: 22,
-        App: 15,
-        Doc: 5,
+        Patients: 22,
+        Appoints: 15,
+        Doctors: 5,
     },
- 
+
 ];
 
 function Chart() {
@@ -84,8 +95,8 @@ function Chart() {
         }
 
         // Start the counter with the specified parameters
-        counter("count1", 0, 1999, 1200);
-        counter("count2", 0, 2999, 2000);
+        counter("count1", 0, 1999, 600);
+        counter("count2", 0, 2999, 800);
 
 
         // Set the count state to the end value
@@ -94,64 +105,154 @@ function Chart() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []); // Empty dependency array ensures the effect runs only once on mount
 
+    const Data = [
+        {
+            avatarSrc: "https://st2.depositphotos.com/45049140/44509/v/450/depositphotos_445090736-stock-illustration-flat-male-doctor-avatar-in.jpg",
+            primaryText: "Brunch this weekend?",
+            secondaryText: "Ali Connors — I'll be in your neighborhood doing errands this…",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Andonia tonight?",
+            secondaryText: "Sorry, I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "DPplans ?",
+            secondaryText: " I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Actonr plans for tonight?",
+            secondaryText: "Steve Smith — Sorry, I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Star Price road",
+            secondaryText: "Sorry, I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Aquaf for tonight?",
+            secondaryText: "Smith — Sorry, ...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Dinner plans for tonight?",
+            secondaryText: "Bob Smith — Sorry, I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Dinner plans for tonight?",
+            secondaryText: "Bob Smith — Sorry, I have other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: "Dinner",
+            secondaryText: " other plans already...",
+        },
+        {
+            avatarSrc: "https://t4.ftcdn.net/jpg/01/34/29/31/360_F_134293169_ymHT6Lufl0i94WzyE0NNMyDkiMCH9HWx.jpg",
+            primaryText: " for tonight?",
+            secondaryText: " — Sorry, I have other plans already...",
+        },
+        // Add more data objects as needed
+    ];
 
     return (
-        <Grid container style={{ position: 'relative' }}>
+        <Grid container >
 
             <Grid item xs={8} style={{ flexWrap: 'wrap' }}>
-                <Grid container item p={2} mt={1} xs={12}>
-
-                    <Grid item xs={6} style={{ transition: 'box-shadow 0.3s' }}>
+                <Grid container item p={2} mt={1} xs={12}  >
+                    <Grid  item xs={6} style={{ transition: 'box-shadow 0.3s'}} >
                         <div className="hov">
-                        <div style={{position:'relative', height: '12rem', border: '1px solid black', marginRight: '1rem', borderRadius: '10px' }}>
-                            <h4 style={{ textAlign:'center',fontSize:'2rem',fontFamily:'mainlux' }}>Patients</h4>
-                            <div style={{position:'absolute', fontSize: '2rem', textAlign:'center',transform:'translate(-50%,-50%)' }} id="count1">99<span>+</span> </div>
+                            <div style={{background: 'linear-gradient(135deg,  #006494,#35CFF4, #35CFF4,#AEE3F0)', height: '10rem',  boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'  ,marginRight: '1rem', borderRadius: '10px' }}>
+                             <Grid style={{display:'flex'}} item xs={12}>
+                             <Grid itme xs={6}>
+                               <h4  style={{ marginBottom:'0',paddingLeft:'2rem',  fontSize: '2rem', fontFamily: 'mainlux' }}>Patients</h4>
+                                <span style={{paddingLeft:'2rem', color:'white', fontSize: '1.52rem' }} id="count1"></span>
+                                <span style={{ color:'white', fontSize: '1.52rem'}}>+</span>
+                               </Grid >
+                               <Grid item xs={6}>
+                              
+                               </Grid>
+                             </Grid>
+                            </div>
                         </div>
-                        </div> 
                     </Grid>
 
                     <Grid className="hov" item xs={6} >
-                        <div style={{ height: '12rem', border: '1px solid black', borderRadius: '10px' }}>
-                            <h4 style={{ textAlign:'center',fontSize:'2rem',fontFamily:'mainlux' }}>Doctors</h4>
-                            <div style={{ fontSize: '2rem', textAlign:'center' }} id="count2">99<span>+</span></div>
+                        <div style={{ background: 'linear-gradient(-35deg,  #006494,#35CFF4, #35CFF4,#AEE3F0)' ,height: '10rem', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' , borderRadius: '10px' }}>
+                        <Grid style={{display:'flex'}} item xs={12}>
+                             <Grid itme xs={6}>
+                             <h4 style={{paddingLeft:'2rem', marginBottom:'0',  fontSize: '2rem', fontFamily: 'mainlux' }}>Doctors</h4>
+                            <span style={{paddingLeft:'2.5rem', color:'white', fontSize: '1.52rem' }} id="count2"></span>
+                            <span style={{ color:'white', fontSize: '1.52rem'}}>+</span>
+
+                               </Grid >
+                               <Grid item xs={6}>
+                                <div  style={{width:'70%',height:'100%',marginTop:'10%',marginLeft:'10%',backgroundColor:'white', paddingTop:'10px',borderRadius:'50%', position:'relative'}}>
+                                <Image style={{position:'absolute',transform:'translate(15%)'}} height={100} width={100}  src={Doc}/>
+                                </div>
+                            
+
+                               </Grid>
+                             </Grid>
+                           
                         </div>
                     </Grid>
                 </Grid>
 
-                <Grid item xs={12} style={{ display: 'flex' }}>
-                    <Grid item xs={12} p={1} >
+                <Grid p={2} item xs={12} style={{ display: 'flex' }}>
+                    <Grid item xs={12} p={2}  style={{
+                                boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',borderRadius: '5px' ,backgroundColor:'#FAFAFA'
+                            }}>
                         <ComposedChart
                             width={680}
-                            height={400}
+                            height={380}
                             data={data}
-                        margin={{
-                          top: 20,
-                          right: 80,
-                          bottom: 20,
-                          left: 20
-                        }}
+                            margin={{
+                                top: 20,
+                                right: 80,
+                                bottom: 20,
+                                left: 20
+                            }}
+                           
                         >
                             <CartesianGrid stroke="#f5f5f5" />
                             <XAxis
                                 dataKey="name"
                                 label={{ value: "Days", position: "insideBottomRight", offset: -10 }}
-                                // scale="band"
+                            // scale="band"
                             />
                             <YAxis label={{ value: "Quantity", angle: -90, position: "insideLeft" }} />
                             <Tooltip />
                             <Legend />
-                            <Area type="monotone" dataKey="App" fill="#8884d8" stroke="#8884d8" />
-                            <Bar dataKey="Pat" barSize={20} fill="#413ea0" />
-                            <Line type="monotone" dataKey="Doc" stroke="#ff7300" />
+                            <Area  type="monotone" dataKey="Appoints" fill="#35CFF4" stroke="#35CFF4" />
+                            <Bar dataKey="Patients" barSize={20} fill="#244C73" />
+                            <Line type="monotone" dataKey="Doctors" stroke="#ff7300" />
                         </ComposedChart>
                     </Grid>
                 </Grid>
             </Grid>
 
-            <Grid item xs={4} p={1} pl={3} >
-                <div style={{ backgroundColor: '#13293D', height: '100%' }}>
+            <Grid item xs={4} p={1} pl={3}  >
+                {/* <div style={{ backgroundColor: '#13293D' }}> */}
 
-                </div>
+                <List style={{ boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',borderRadius: '5px', overflowY: 'scroll', height: 'calc(100vh - 90px)', backgroundColor: '#13293D' }} className='Colo' sx={{ width: '100%', maxWidth: 360 }}>
+
+                    <h2 className='Colo' style={{ textAlign: 'center' }}>Appointments</h2>
+                    {Data.map((item, index) => (
+                        <CommonListItem
+                            key={index}
+                            avatarSrc={item.avatarSrc}
+                            primaryText={item.primaryText}
+                            secondaryText={item.secondaryText}
+                        />
+                    ))}
+
+                </List>
+                {/* </div> */}
             </Grid>
         </Grid>
 
