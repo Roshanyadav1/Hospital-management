c4a172bccd44e6fcfac8484bb3a93298d0e8e95f
Prashant-Y <prashantydv2409@gmail.com>
2023-12-04 10:41:13 +0530

    Dynaic Chart

--- frontend/hospital_management/src/components/Chart.js
+++ frontend/hospital_management/src/components/Chart.js
@@ -21,52 +21,52 @@ import CommonListItem from './CommonListItem';
 import Image from 'next/image'
 import Doc from './Doc.png'
 
-const data = [
-    {
-        name: "Sun",
-        Patients: 29,
-        Appoints: 25,
-        Doctors: 6,
-    },
-    {
-        name: "Mon",
-        Patients: 32,
-        Appoints: 32,
-        Doctors: 11,
-    },
-    {
-        name: "Tues",
-        Patients: 38,
-        Appoints: 23,
-        Doctors: 4,
-    },
-    {
-        name: "Wed",
-        Patients: 32,
-        Appoints: 27,
-        Doctors: 7,
-    },
+// const data = [
+//     {
+//         name: "Sun",
+//         Patients: 29,
+//         Appoints: 25,
+//         Doctors: 6,
+//     },
+//     {
+//         name: "Mon",
+//         Patients: 32,
+//         Appoints: 32,
+//         Doctors: 11,
+//     },
+//     {
+//         name: "Tues",
+//         Patients: 38,
+//         Appoints: 23,
+//         Doctors: 4,
+//     },
+//     {
+//         name: "Wed",
+//         Patients: 32,
+//         Appoints: 27,
+//         Doctors: 7,
+//     },
 
-    {
-        name: "Thurs",
-        Patients: 22,
-        Appoints: 15,
-        Doctors: 3,
-    },
-    {
-        name: "Fri",
-        Patients: 26,
-        Appoints: 19,
-        Doctors: 9,
-    },
-    {
-        name: "Sat",
-        Patients: 22,
-        Appoints: 15,
-        Doctors: 7,
-    },
+//     {
+//         name: "Thurs",
+//         Patients: 22,
+//         Appoints: 15,
+//         Doctors: 3,
+//     },
+//     {
+//         name: "Fri",
+//         Patients: 26,
+//         Appoints: 19,
+//         Doctors: 9,
+//     },
+//     {
+//         name: "Sat",
+//         Patients: 22,
+//         Appoints: 15,
+//         Doctors: 7,
+//     },
 
-];
+// ];
 
 function Chart() {
 
@@ -119,6 +119,10 @@ function Chart() {
     }
 
     return {
+      name: appointment.appointement_per_week.appointment_date,
+      Patients: appointment.patient_count,
+      Appoints: appointment.appointment_count,
+      Doctors: appointment.doctor_count,
       avatarSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMjX02hunzz3i3dG7PG7J2AM61C5AVahSHBg&usqp=CAU",
       primaryText: appointment.doctor.employee.employee_name,
       secondaryText: `Appointment Date: ${appointment.appointment_date}`,
@@ -126,6 +130,7 @@ function Chart() {
       patient_name: `Patient Name: ${appointment.patient.patient_name}`,
     };
 });
+console.log("Data for Chart:", Data);
 
     // const Data = [
     //     {
@@ -232,8 +237,8 @@ function Chart() {
                             }}>
                         <ComposedChart
                             width={650}
-                            height={420}
-                            data={data}
+                            height={355}
+                            data={Data}
                             margin={{
                                 top: 20,
                                 right: 80,
@@ -288,3 +293,188 @@ function Chart() {
 }
 
 export default Chart
+
+//from this code given by you the graph chart is not taking the correct appointment_date, appointment_count, doctor_count, patient_count from the appointement_per_week from API from the I have provide it is taking the wrong date and the other data is not displayed correctly. So, correct this thing I have told you and take the data dynamically for the graph chart and display it correctly on the graph
+
+// {
+//   "status": 200,
+//   "message": "Appointment Data Retrieved Successfully",
+//   "appointement_per_week": [
+//     {
+//       "appointment_date": "2023-11-29",
+//       "appointment_count": 1,
+//       "doctor_count": 1,
+//       "patient_count": 1
+//     },
+//     {
+//       "appointment_date": "2023-11-30",
+//       "appointment_count": 2,
+//       "doctor_count": 2,
+//       "patient_count": 2
+//     },
+//     {
+//       "appointment_date": "2023-12-01",
+//       "appointment_count": 1,
+//       "doctor_count": 1,
+//       "patient_count": 1
+//     },
+//     {
+//       "appointment_date": "2023-12-03",
+//       "appointment_count": 1,
+//       "doctor_count": 1,
+//       "patient_count": 1
+//     }
+//   ],
+//   "data": [
+//     {
+//       "appointment_id": "dd26fb07-b855-46d8-bcae-63c221210230",
+//       "appointment_number": 2,
+//       "appointment_time": "23:02:00",
+//       "appointment_date": "2023-12-03",
+//       "doctor": {
+//         "doctor_id": "c3a111d2-1009-4a11-a45e-b551b56a66ca",
+//         "disease_specialist": "[\"Dengue\", \"Diabetes\"]",
+//         "times": "[[\"09:00:00\", \"12:00:00\"], [\"02:00:00\", \"05:00:00\"], [\"07:00:00\", \"10:00:00\"]]",
+//         "day": "[\"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+//         "per_patient_time": "00:30:00",
+//         "status": "null",
+//         "employee": {
+//           "employee_name": "Prashant Yadav"
+//         }
+//       },
+//       "patient": {
+//         "patient_id": "b1ebabba-6f65-4bbf-a3ca-f48e448a7d91",
+//         "patient_name": "Tony Stark",
+//         "patient_age": 25,
+//         "patient_address": "Indore",
+//         "patient_email": "tony@gmail.com",
+//         "patient_mobile": 7894561231
+//       },
+//       "disease": {
+//         "disease_id": "7730918a-9fa9-49a5-919b-dd403309b980",
+//         "disease_name": "Anxiety disorders",
+//         "disease_status": "Active"
+//       }
+//     },
+//     {
+//       "appointment_id": "48b34a04-b470-4b1c-8efe-2202cc6f7510",
+//       "appointment_number": 3,
+//       "appointment_time": "01:11:00",
+//       "appointment_date": "2023-11-29",
+//       "doctor": {
+//         "doctor_id": "c3a111d2-1009-4a11-a45e-b551b56a66ca",
+//         "disease_specialist": "[\"Dengue\", \"Diabetes\"]",
+//         "times": "[[\"09:00:00\", \"12:00:00\"], [\"02:00:00\", \"05:00:00\"], [\"07:00:00\", \"10:00:00\"]]",
+//         "day": "[\"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+//         "per_patient_time": "00:30:00",
+//         "status": "null",
+//         "employee": {
+//           "employee_name": "Prashant Yadav"
+//         }
+//       },
+//       "patient": {
+//         "patient_id": "b1ebabba-6f65-4bbf-a3ca-f48e448a7d91",
+//         "patient_name": "Tony Stark",
+//         "patient_age": 25,
+//         "patient_address": "Indore",
+//         "patient_email": "tony@gmail.com",
+//         "patient_mobile": 7894561231
+//       },
+//       "disease": {
+//         "disease_id": "45acaee0-dcfb-4f57-818b-380e11bd9b4a",
+//         "disease_name": "Chest pain",
+//         "disease_status": "Active"
+//       }
+//     },
+//     {
+//       "appointment_id": "b62d4201-052f-46d8-aa37-5ccfb8eeb564",
+//       "appointment_number": 5,
+//       "appointment_time": "00:16:00",
+//       "appointment_date": "2023-11-30",
+//       "doctor": {
+//         "doctor_id": "cea11f46-3bb9-410b-9460-ef57f9a0f600",
+//         "disease_specialist": "[\"Asthma\", \"Common cold\"]",
+//         "times": "[[\"09:00:00\", \"12:00:00\"], [\"02:00:00\", \"05:00:00\"], [\"07:00:00\", \"10:00:00\"]]",
+//         "day": "[\"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+//         "per_patient_time": "00:30:00",
+//         "status": "null",
+//         "employee": {
+//           "employee_name": "Piyush Dixit"
+//         }
+//       },
+//       "patient": {
+//         "patient_id": "b1ebabba-6f65-4bbf-a3ca-f48e448a7d91",
+//         "patient_name": "Tony Stark",
+//         "patient_age": 25,
+//         "patient_address": "Indore",
+//         "patient_email": "tony@gmail.com",
+//         "patient_mobile": 7894561231
+//       },
+//       "disease": {
+//         "disease_id": "72d9291c-f119-46f3-b0ed-44ff32697320",
+//         "disease_name": "Asthma",
+//         "disease_status": "Active"
+//       }
+//     },
+//     {
+//       "appointment_id": "47dabbe1-1f7b-4937-be87-9dc16586f692",
+//       "appointment_number": 1,
+//       "appointment_time": "11:29:00",
+//       "appointment_date": "2023-11-30",
+//       "doctor": {
+//         "doctor_id": "c3a111d2-1009-4a11-a45e-b551b56a66ca",
+//         "disease_specialist": "[\"Dengue\", \"Diabetes\"]",
+//         "times": "[[\"09:00:00\", \"12:00:00\"], [\"02:00:00\", \"05:00:00\"], [\"07:00:00\", \"10:00:00\"]]",
+//         "day": "[\"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+//         "per_patient_time": "00:30:00",
+//         "status": "null",
+//         "employee": {
+//           "employee_name": "Prashant Yadav"
+//         }
+//       },
+//       "patient": {
+//         "patient_id": "5b3001af-6c37-4a88-8e86-c7c234989aa6",
+//         "patient_name": "John Doe",
+//         "patient_age": 30,
+//         "patient_address": "123 Main St, Cityville",
+//         "patient_email": "john.doe@example.com",
+//         "patient_mobile": 1234567890
+//       },
+//       "disease": {
+//         "disease_id": "0923cb6a-14a6-46ea-9f9f-d3830d869f8f",
+//         "disease_name": "Chronic kidney disease",
+//         "disease_status": "Active"
+//       }
+//     },
+//     {
+//       "appointment_id": "c040b47b-3120-474e-80ba-99a8840d8313",
+//       "appointment_number": 4,
+//       "appointment_time": "11:31:00",
+//       "appointment_date": "2023-12-01",
+//       "doctor": {
+//         "doctor_id": "cea11f46-3bb9-410b-9460-ef57f9a0f600",
+//         "disease_specialist": "[\"Asthma\", \"Common cold\"]",
+//         "times": "[[\"09:00:00\", \"12:00:00\"], [\"02:00:00\", \"05:00:00\"], [\"07:00:00\", \"10:00:00\"]]",
+//         "day": "[\"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+//         "per_patient_time": "00:30:00",
+//         "status": "null",
+//         "employee": {
+//           "employee_name": "Piyush Dixit"
+//         }
+//       },
+//       "patient": {
+//         "patient_id": "5b3001af-6c37-4a88-8e86-c7c234989aa6",
+//         "patient_name": "John Doe",
+//         "patient_age": 30,
+//         "patient_address": "123 Main St, Cityville",
+//         "patient_email": "john.doe@example.com",
+//         "patient_mobile": 1234567890
+//       },
+//       "disease": {
+//         "disease_id": "0923cb6a-14a6-46ea-9f9f-d3830d869f8f",
+//         "disease_name": "Chronic kidney disease",
+//         "disease_status": "Active"
+//       }
+//     }
+//   ]
+// }
