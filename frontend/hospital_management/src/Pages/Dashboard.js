a78b5cef40637c27c8147f95068b96cd7f2b4d8d
Nileshgiri17 <nilesh@ssism.org>
2023-12-07 20:29:19 +0530

    changes

--- frontend/hospital_management/src/Pages/Dashboard.js
+++ frontend/hospital_management/src/Pages/Dashboard.js
@@ -1,22 +1,246 @@
-"use client"
-import { useRouter } from 'next/navigation'
-import DataGridTable from './DataGridTable';
-import { useGetEmployeeQuery } from '@/services/Query';
-import { columns } from '@/data/ColumData';
-import DataTable from './DataTable';
+'use client'
+import DataGridTable from './DataGridTable'
+import { useAddEmployeeMutation, useGetEmployeeQuery } from '@/services/Query'
+import { columns } from '@/data/ColumData'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { Formik, Form} from 'formik';
+import { Employee_Validation } from '../components/FormValidation/FormValidation';
+import Textfield from '../components/Textfield/Text'
+import RadioButtonGroup from '@/components/RadioB/RadioButtonGroup'
+import { Divider } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import CustomAutocomplete from '@/components/Autocomplete'
 
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+const INITIAL_FORM_STATE = {
+  employee_name: '',
+  employee_email: '',
+  employee_number: '',
+  employee_password: '',
+  employee_type: '',
+  employee_role: '',
+  employee_status: '',
+  created_by: 'admin',
+  updated_by: 'admin',
+};
+
+const Empcategories = ['Part Time','Full Time'];
+const Role = ['Doctor','Manager'];
 
 function Dashboard() {
-  // const router = useRouter()
-  const { data: empData } = useGetEmployeeQuery()
+   const [open, setOpen] = useState(false)
+   const { data: empData } = useGetEmployeeQuery()
+  const [addemployee] = useAddEmployeeMutation()
 
-  return (
-    <>
-          <DataGridTable        
-            data={empData?.data || []}
-            columns={columns} />
-    </>
-  )
+   const handleClickOpen = () => {
+      setOpen(true)
+   }
+   const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleRegister = async (values,{resetForm}) => {
+    try {
+      await addemployee(values);
+      resetForm();
+    } catch (error) {
+      // Handle error
+      // console.error('Error submitting form:', error);
+    }
+
+  }
+
+   return (
+      <Box sx={{ flexGrow: 1 }}>
+         <Grid
+            container
+            direction='row'
+            justifyContent='space-between'
+            alignItems='center'
+            mt={4}
+         >
+            <Typography variant='h3' component='div' sx={{ flexGrow: 1 }}>
+               Employee
+            </Typography>
+            <Grid>
+               <Button variant='outlined' onClick={handleClickOpen}>
+                  Add Employee
+               </Button>
+            </Grid>
+            <Grid container mt={2}>
+               <Grid item xs={12}>
+                  <DataGridTable
+                     data={empData?.data || []}
+                     columns={columns}
+                     map_by={row => row.employee_id}
+                  />
+               </Grid>
+            </Grid>
+         </Grid>
+         <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Employee Registration Form</DialogTitle>
+            <IconButton
+               aria-label='close'
+               onClick={handleClose}
+               sx={{
+                  position: 'absolute',
+                  right: 8,
+                  top: 8,
+                  color: theme => theme.palette.grey[500],
+               }}
+            >
+               <CloseIcon />
+            </IconButton>
+            <DialogContent>
+            <Formik
+          initialValues={{
+            ...INITIAL_FORM_STATE,
+          }}
+          validationSchema={Employee_Validation}
+          onSubmit={handleRegister}
+        >
+
+          {({ values, handleChange, handleBlur, touched}) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} >
+                  <Textfield name="employee_name" label="Name" autoComplete=""
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Textfield name="employee_email" label="Email" autoComplete="off"
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Textfield name="employee_number" label="Phone" autoComplete="off"
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Textfield name="employee_password" label="Password" autoComplete="off"
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid>
+                
+                <Grid item xs={12} sm={6}>
+                  <CustomAutocomplete
+                    name="employee_type"
+                    label="Employee Type"
+                    options={Empcategories}
+                    value={values.employee_type}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    touched={touched.employee_type}
+                  />
+                </Grid>
+               
+                <Grid item xs={12} sm={6}>
+                  <CustomAutocomplete
+                    name="employee_role"
+                    label="Employee Role"
+                    options={Role}
+                    value={values.employee_role}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    touched={touched.employee_role}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <RadioButtonGroup
+                    label="Status"
+                    name="employee_status"
+                    options={[
+                      { value: 'Available', label: 'Active' },
+                      { value: 'Unavailable', label: 'Inactive' },
+                    ]}
+                  />
+                </Grid>
+                <Divider />
+                {/* <Grid item xs={12} sm={8} >
+                  <Text name="created_by" label="Created By" autoComplete=""
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid> */}
+
+                {/* <Grid item xs={12} sm={6}>
+                  <VisuallyHiddenInput id="logoInput" type='file' accept='image/*' />
+                </Grid>
+
+                <Grid item xs={12} sm={8} >
+                  <Text name="updated_by" label="Updated By" autoComplete=""
+                    InputProps={{
+                      style: {
+                        background: 'white', border: 'none', borderRadius: '20px',
+                      },
+                    }}
+                  />
+                </Grid> */}
+
+                <Grid item xs={12} sm={5}>
+                  <VisuallyHiddenInput id="logoInput" type='file' accept='image/*' />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    size='large'
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+            </DialogContent>
+         </Dialog>
+      </Box>
+   )
 }
 
-export default Dashboard;
+export default Dashboard
--- frontend/hospital_management/src/Pages/DataGridTable.js
+++ frontend/hospital_management/src/Pages/DataGridTable.js
@@ -1,89 +1,18 @@
 "use client"
 import { DataGrid } from '@mui/x-data-grid';
 
-
-
-const customstyles=`
-.MuiDataGrid-root .MuiDataGrid-row:nth-child(even) {
-  background-color: #E6E8F0;
-}
-.MuiDataGrid-root .MuiDataGrid-row:nth-child(odd) {
-  background-color: #f0f2fa;
-}
-  .header{
-    background-color: #006494 !important;
-    color: white;
-    border-right: 3px solid #89949E !important; 
-    border-bottom: 3px solid #89949E !important;
-    font-size: .8rem;
-    font-family: Verdana
-
-  }
-  .headerlast{
-    background-color: #006494 !important;
-    color: white;
-    border-radius:'10px !important';
-    border-bottom: 3px solid #89949E !important;
-    font-size: .8rem;
-    font-family: Verdana;
-  }
-  .column-line {
-    border-right: 1px solid #89949E !important; // Adjust the color as needed
-    padding-right: 8px; // Add padding to separate the text from the line
-  }
-  .css-jv5jpm-MuiDataGrid-root .MuiDataGrid-columnSeparator{
-    display: none;
-  }
-  .MuiDataGrid-footerContainer.MuiDataGrid-withBorderColor {
-    display: none;
-  }
-  .MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation24.MuiDialog-paper.MuiDialog-paperScrollPaper.MuiDialog-paperWidthSm.css-1t1j96h-MuiPaper-root-MuiDialog-paper {
-    height: auto;
-    border: 1px solid white;
-    border-radius: 12px;
-    box-shadow: box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-  .css-1yosu3z-MuiTypography-root-MuiDialogTitle-root{
-    font-size : 1.5rem !important
-  }
-  .Data{
-    font-weight:bold
-  }
-  .No{
-    background-color: rgba(19, 41, 61, 0.04) !important;
-  }
-  .css-1kyp2zt-MuiButtonBase-root-MuiButton-root:hover {
-    color:white
-  }
-  .No:hover{
-   color: black !important
-  }
-  
-`
-
 export default function DataGridTable(props) {
-    const {data,columns} = props
+    const {data, columns, map_by} = props
     
   return (
     <>
-        <style>{customstyles}</style>
         <DataGrid
           rows={data?.length > 0 ? data : []}
           columns={columns}
           loading={data?.length > 0 ? false : true}
-          getRowId={(row) => row?.employee_id}
-          getRowHeight={() => 'auto'}
-
-          //only table scroll not the scrreen
+          getRowId={map_by}
           autoHeight={true}
-
           checkboxSelection={false}
-          rowHeight={35}
-          sx={{
-            height: 392, 
-            display:'flex',
-            width:'100%',
-          }}
           columnHeaderHeight={40}
           disableColumnMenu 
           />
@@ -91,8 +20,3 @@ export default function DataGridTable(props) {
    
   );
 }
-
-
-// insert the delete insert and view buttons in every row of the data in Actions column 
-// fix the size of the table to 8 rows with the pagination in it and when the other page will be opened then the size of the table should renains the same and the left over data should be shown on the rows and the other rows should be displayed wheteher it is fillrf or not
-// below the employee table heading provide one more heading that is 'Employees data is viewed here. You can view, delete & edit ' in h5 with proper allignment 
