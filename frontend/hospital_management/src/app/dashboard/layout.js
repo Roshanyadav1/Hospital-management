d0ad14aecb75ac94dfa798157450af312be4e0f3
roshan <roshan.yadav@baelworks.com>
2023-12-10 04:38:16 +0530

    revert changes

--- frontend/hospital_management/src/app/dashboard/layout.js
+++ frontend/hospital_management/src/app/dashboard/layout.js
@@ -291,5 +291,5 @@ function Layout({ children }) {
    )
 }
 
-// export default withRoleRedirect(Layout, ['Admin', 'Manager'])
-export default Layout
+export default withRoleRedirect(Layout, ['Admin', 'Manager'])
+// export default Layout
