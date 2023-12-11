19f0fcb805c5f868591f0e88300aeb594267d67f
Aman Yadav <virajgurjar789@gmail.com>
2023-12-11 16:31:07 +0530

    some changes

--- frontend/hospital_management/src/helpers/withRoleRedirect.js
+++ frontend/hospital_management/src/helpers/withRoleRedirect.js
@@ -6,13 +6,13 @@ const withRoleRedirect = (WrappedComponent, allowedRoles) => {
    const WithRoleRedirect = (props) => {
       const router = useRouter()
       const userRole = localStorage.getItem('user_role')
-
+   
       useLayoutEffect(() => {
-         // Check if user role is in the allowedRoles array
+        
          if (!userRole && !allowedRoles.includes(userRole)) {
             router.push('/') // Change this to your default login page
          }
-      }, [router])
+      }, [router,userRole, allowedRoles])
 
 
       if (userRole && allowedRoles.includes(userRole)) {
