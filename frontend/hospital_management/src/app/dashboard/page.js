e9dca9a9d430854b5ff11cb2a62fe9141b03d89f
Megha <meghadhangar1301@gmail.com>
2023-12-12 10:38:48 +0530

    changes

--- frontend/hospital_management/src/app/dashboard/page.js
+++ frontend/hospital_management/src/app/dashboard/page.js
@@ -1,107 +1,27 @@
-// 'use client'
-// import Chart from '@/Pages/Chart'
-// import Card from '@mui/material/Card'
-// import CardHeader from '@mui/material/CardHeader'
-// import CardContent from '@mui/material/CardContent'
-// import Avatar from '@mui/material/Avatar'
-// import { useGetAppointmentQuery } from '@/services/Query'
-// import { Chip, Container, Grid, } from '@mui/material'
-// const userRole = 'admin'
-// function FetchData() {
-//    const { data: appointment, isLoading, isError } = useGetAppointmentQuery()
-        
-   
-//    return (
-//       <div>
-//         {userRole === 'Admin' || userRole === 'Manager' ? (
-//             <Chart />
-//          ) : null}
 
-
-//       <Container maxWidth='sm'>
-//          <Grid mt={2} container spacing={2}>
-//             {Array.isArray(appointment?.data) &&
-//                appointment?.data?.map((e, i) => (
-//                   <Grid
-//                      item
-//                      key={i}
-//                      xs={12}
-//                      sm={12}
-//                      md={12}
-//                      lg={12}
-//                      sx={{ paddingBottom: '1rem' }}
-//                   >
-//                      <Card sx={{ backgroundColor: '#C4D0DC' }}>
-//                         <CardHeader
-//                            avatar={
-//                               <Avatar
-//                                  sx={{ bgcolor: '#13293D' }}
-//                                  aria-label='recipe'
-//                               >
-//                                  {e?.doctor?.employee?.employee_name.split('')[0]}
-//                               </Avatar>
-//                            }
-//                            title={e?.doctor?.employee?.employee_name}
-//                            subheader={
-//                               e?.appointment_time + '   ' + e?.appointment_date
-//                            }
-//                         />
-//                         <CardContent>
-//                            <Grid
-//                               container
-//                               spacing={1}
-//                               alignItems='center'
-//                               justifyContent='space-between'
-//                            >
-//                               <Grid item>
-//                                     <Chip
-//                                        label={e?.disease?.disease_name}
-//                                        sx={{ backgroundColor: '#7F8FA45B', marginRight: 1 }}
-//                                     />
-//                               </Grid>
-//                            </Grid>
-//                         </CardContent>
-//                      </Card>
-//                   </Grid>
-//                ))}
-//          </Grid>
-//       </Container>
-
-
-//       </div>
-//    )
-// }
-
-// export default FetchData
-
-
-
-// ... (existing imports)
 
 import Chart from '@/Pages/Chart';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
-// import { useGetAppointmentQuery } from '@/services/Query';
+import { useGetAppointmentQuery } from '@/services/Query';
 import { Chip, Container, Grid } from '@mui/material';
-
+ 
 const userRole = 'Doctor';
+// const userRole = localStorage.getItem('role');
 
 function FetchData() {
-   // const { data: appointment, isLoading, isError } = useGetAppointmentQuery();
-   let appointment = []
-
+   const { data: appointment, isLoading, isError } = useGetAppointmentQuery();
    return (
       <div>
          {userRole === 'Admin' || userRole === 'Manager' ? (
             <Chart />
          ) : userRole === 'Doctor' ? (
-            // Add content specific to doctors
+            
             <Container maxWidth='sm'>
                <Grid mt={2} container spacing={2}>
-                  {Array.isArray(appointment?.data) &&
-                     appointment?.data?.map((e, i) => (
+         
                         <Grid
                            item
                            key={i}
@@ -118,13 +38,10 @@ function FetchData() {
                                        sx={{ bgcolor: '#13293D' }}
                                        aria-label='recipe'
                                     >
-                                       {e?.doctor?.employee?.employee_name.split('')[0]}
+                                    <h2>meghaa</h2>
                                     </Avatar>
                                  }
-                                 title={e?.doctor?.employee?.employee_name}
-                                 subheader={
-                                    e?.appointment_time + '   ' + e?.appointment_date
-                                 }
+
                               />
                               <CardContent>
                                  <Grid
@@ -133,17 +50,17 @@ function FetchData() {
                                     alignItems='center'
                                     justifyContent='space-between'
                                  >
-                                    <Grid item>
+                                    {/* <Grid item>
                                        <Chip
                                           label={e?.disease?.disease_name}
                                           sx={{ backgroundColor: '#7F8FA45B', marginRight: 1 }}
                                        />
-                                    </Grid>
+                                    </Grid> */}
                                  </Grid>
                               </CardContent>
                            </Card>
                         </Grid>
-                     ))}
+               
                </Grid>
             </Container>
          ) : null}
