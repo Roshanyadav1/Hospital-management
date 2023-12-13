2f89d0a32f7b27812fa7fdbf887df3ae840f1ea5
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-12-13 11:12:37 +0530

    user delete fixed

--- backend/employee/views.py
+++ backend/employee/views.py
@@ -193,16 +193,12 @@ class EmployeeDelete(APIView):
         id = input
         if Employee.objects.filter(employee_id=id).count() >= 1:
             employee = Employee.objects.get(employee_id=id)
-            # user = ""
-            # if employee.employee_role == 'Manager':
-            #     user = User.objects.get(member_id=id)
-
-            # if employee.employee_role == 'Doctor':
-            #     doctor = Doctor.objects.get(employee_id=id)
-            #     doctor.delete()
-            #     user = User.objects.get(member_id=doctor.doctor_id)
-            #     user.delete()
-            # user.delete()
+            user = ""
+            try:
+                user = User.objects.get(member_id=id)
+                user.delete()
+            except:
+                pass
             employee.delete()
             response_message = ""
             response_code = ""
