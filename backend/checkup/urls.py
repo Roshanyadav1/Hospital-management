ad7f58a8c278ae2fc6ad71be2b0cdd092980978f
Keshav Agrawal <keshava.bca2022@ssism.org>
2023-12-13 17:11:07 +0530

    trailing slashes removed

--- backend/checkup/urls.py
+++ backend/checkup/urls.py
@@ -3,9 +3,9 @@ from checkup.views import *
 
 
 urlpatterns= [
-    path('add/', CheckUpAdd.as_view(), name='checkup add'),
+    path('add', CheckUpAdd.as_view(), name='checkup add'),
     path('view/',CheckUpView.as_view(), name = 'checkup view'),
     path('view/<uuid:input>/', CheckUpViewById.as_view(), name = 'checkup view by id'),
-    path('delete/<uuid:input>/', CheckUpDelete.as_view(), name = 'checkup delete'),
-    path('update/<uuid:input>/', CheckUpUpdate.as_view(), name = 'checkup update')
+    path('delete/<uuid:input>', CheckUpDelete.as_view(), name = 'checkup delete'),
+    path('update/<uuid:input>', CheckUpUpdate.as_view(), name = 'checkup update')
 ]
--- backend/leave/urls.py
+++ backend/leave/urls.py
@@ -3,7 +3,7 @@ from leave.views import *
 
 
 urlpatterns = [
-    path('add/', LeaveRegister.as_view(), name = 'leave add'),
+    path('add', LeaveRegister.as_view(), name = 'leave add'),
     path('view/', LeaveView.as_view(), name = 'leave profile view'),
     path('view/<uuid:input>', LeaveViewById.as_view(), name = 'leave profile view by id'),
     path('update/<uuid:input>', LeaveUpdate.as_view(), name = 'leave profile update'),
