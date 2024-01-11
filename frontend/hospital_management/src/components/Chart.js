f6326016d0cb0f74f448ab8f6a9af591deee0562
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-01-11 10:38:23 +0530

    spell fixed

--- frontend/hospital_management/src/components/Chart.js
+++ frontend/hospital_management/src/components/Chart.js
@@ -99,7 +99,7 @@ function Chart() {
       return {
          name: appointment.appointment_date,
          Patients: appointment.patient_count,
-         Appoints: appointment.appointment_count,
+         Appointments: appointment.appointment_count,
          Doctors: appointment.doctor_count,
       }
    })
@@ -116,7 +116,7 @@ function Chart() {
       return {
          name: appointment.appointment_date,
          Patients: appointment.patient_count,
-         Appoints: appointment.appointment_count,
+         Appointments: appointment.appointment_count,
          Doctors: appointment.doctor_count,
          avatarSrc:
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMjX02hunzz3i3dG7PG7J2AM61C5AVahSHBg&usqp=CAU',
@@ -154,35 +154,35 @@ function Chart() {
                            }}
                         >
                            <Grid style={{ display: 'flex' }} item xs={12}>
-                                             <Card sx={{ display: 'flex' }}>
-                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <CardContent sx={{ flex: '1 0 auto' }}>
-                           <Typography component="div" variant="h5">
-                              Live From Space
-                           </Typography>
-                           <Typography variant="subtitle1" color="text.secondary" component="div">
-                              Mac Miller
-                           </Typography>
-                        </CardContent>
-                        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                           <IconButton aria-label="previous">
-                              {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-                           </IconButton>
-                           <IconButton aria-label="play/pause">
-                              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                           </IconButton>
-                           <IconButton aria-label="next">
-                              {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-                           </IconButton>
-                        </Box>
-                        </Box>
-                        <CardMedia
-                        component="img"
-                        sx={{ width: 151 }}
-                        image="/static/images/cards/live-from-space.jpg"
-                        alt="Live from space album cover"
-                        />
-                     </Card>
+                              <Card sx={{ display: 'flex' }}>
+                                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                    <CardContent sx={{ flex: '1 0 auto' }}>
+                                       <Typography component="div" variant="h5">
+                                          Live From Space
+                                       </Typography>
+                                       <Typography variant="subtitle1" color="text.secondary" component="div">
+                                          Mac Miller
+                                       </Typography>
+                                    </CardContent>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                       <IconButton aria-label="previous">
+                                          {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+                                       </IconButton>
+                                       <IconButton aria-label="play/pause">
+                                          <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                                       </IconButton>
+                                       <IconButton aria-label="next">
+                                          {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+                                       </IconButton>
+                                    </Box>
+                                 </Box>
+                                 <CardMedia
+                                    component="img"
+                                    sx={{ width: 151 }}
+                                    image="/static/images/cards/live-from-space.jpg"
+                                    alt="Live from space album cover"
+                                 />
+                              </Card>
                            </Grid>
                         </div>
                      </div>
@@ -281,7 +281,7 @@ function Chart() {
                            position: 'insideBottomRight',
                            offset: -10,
                         }}
-                        // scale="band"
+                     // scale="band"
                      />
                      <YAxis
                         label={{
@@ -294,7 +294,7 @@ function Chart() {
                      <Legend />
                      <Area
                         type='monotone'
-                        dataKey='Appoints'
+                        dataKey='Appointments'
                         fill='#AEE3F0'
                         stroke='#AEE3F0'
                      />
