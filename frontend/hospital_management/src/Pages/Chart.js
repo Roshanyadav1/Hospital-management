95c46a40744e47a900567c66524b386ebfdde529
harshita-ydv <harshitay.bba2022@ssism.org>
2024-01-13 09:33:32 +0530

    changes

--- frontend/hospital_management/src/Pages/Chart.js
+++ frontend/hospital_management/src/Pages/Chart.js
@@ -111,7 +111,7 @@ function Chart() {
          Doctors: appointment.doctor_count,
          avatarSrc:
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMjX02hunzz3i3dG7PG7J2AM61C5AVahSHBg&usqp=CAU',
-         primaryText: appointment.doctor.employee.employee_name,
+         primaryText: appointment?.doctor?.employee.employee_name,
          secondaryText: `Appointment Date: ${appointment.appointment_date}`,
          disease_names: `Disease Name: ${appointment.disease.disease_name}`,
          patient_name: `Patient Name: ${appointment.patient.patient_name}`, // Corrected line
