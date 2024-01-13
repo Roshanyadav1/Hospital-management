80b5fe823dbd4587c748a9bbd8a85566a0e5d237
harshita-ydv <harshitay.bba2022@ssism.org>
2024-01-13 16:56:46 +0530

    download and add images on landingpage

--- frontend/hospital_management/src/helpers/welcome.js
+++ frontend/hospital_management/src/helpers/welcome.js
@@ -1,50 +1,52 @@
+
 export const welcome = [
    {
-      title: ' Neurosiences',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/neurosciences-04.png',
+      title: 'Neurosciences',
+      image: '/disease/neurosciences-1.png',
       description: 'The Department of Neurology at Care',
-      category: 'Neurosiences',
+      category: 'Neurosciences',
    },
    {
-      title: ' Transplant',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/transplants-12.png',
-      description: 'The centre of transparant offers in hispital',
+      title: 'Transplant',
+      image: '/disease/transplants-2.png',
+      description: 'The center of transparent offers in hospital',
       category: 'Transplant',
    },
    {
-      title: ' Care Unit',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/icons_critical%20care%20medicine.png',
-      description: 'The centre caters to critical patients..',
+      title: 'Care Unit',
+      image: '/disease/care medicine3.png',
+      description: 'The center caters to critical patients..',
       category: 'Care Unit',
    },
    {
       title: 'Cardiology',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/coe-1.png',
+      image: '/disease/coe-4.png',
       description: 'Our department of Cardiology in hospital',
       category: 'Cardiology',
    },
    {
       title: 'Orthopaedics',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/orthopedics-06.png',
+      image: '/disease/orthopedics-5.png',
       description: 'The Orthopaedics Department of CARE CHL..',
       category: 'Orthopaedics',
    },
    {
       title: 'Gynaecology',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/women%20and%20child%20institute-11.png',
-      description: 'The Centreof obstetrics and Gynaecology..',
+      image: '/disease/women6.png',
+      description: 'The Center of Obstetrics and Gynaecology..',
       category: 'Gynaecology',
    },
    {
       title: 'Paediatrics',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/pediatrics-07.png',
+      image: '/disease/pediatrics-7.png',
       description: 'The Department of Paediatrics at CARE CHL',
       category: 'Paediatrics',
    },
    {
-      title: 'Neurosiences',
-      image: 'https://www.carehospitals.com/indore/assets/images/speciality/surgical%20oncology.png',
+      title: 'Neurosciences',
+      image: '/disease/surgical8.png',
       description: 'The Department of Neurology at Care CHL',
-      category: 'Neurosiences',
+      category: 'Neurosciences',
    },
-]
+];
+
